c9f8bd1f0b073e44f3980dc745449681c8728a2f
shibilabasheer <shibila.abasheer@gmail.com>
2025-11-10 01:17:45 +0530

    payment

--- server/models/Payment.js
+++ server/models/Payment.js
@@ -5,7 +5,7 @@ const paymentSchema = new mongoose.Schema({
   order: { type:Schema.Types.ObjectId, ref: 'Order' },
   provider: { type: String, enum: ['stripe','paypal','cod','wallet','other'], default: 'stripe' },
   amount: { type: Number, required: true },
-  status: { type: String, enum: ['pending','succeeded','failed','refunded'], default: 'pending' },
+  status: { type: String, enum: ['pending','paid','failed','refunded'], default: 'pending' },
   transactionId: { type: String }
 }, { timestamps: true });
 
